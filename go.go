4ac65ebd6e6eaa6aad5195193acdd448695da7ce
Marin Procureur <marin.procureur@enioka.com>
2016-05-12 23:53:51 +0200

    Stable version with search job from id

--- go.go
+++ go.go
@@ -11,18 +11,17 @@ import (
 
 type (
 	MyJob struct {
+		name string
 		commandName string
+		arguments []string
 	}
 )
 
 func (myjob *MyJob) ExecuteJob() (err *gjp.JobError) {
-	out, error := exec.Command("echo","toto").Output()
-	if error != nil {
-		defer panic("plz send haelp")
-		err = &gjp.JobError{
-			error.Error(),
-			"nooooooooo",
-		}
+	out, commandError := exec.Command("echo","toto").Output()
+	if commandError != nil {
+		defer panic(commandError.Error())
+		err = gjp.NewJobError(commandError)
 		return
 	}
 	fmt.Printf("The date is %s\n", out)
@@ -45,28 +44,51 @@ func main() {
 		jobPool.ShutdownWorkPool()
 	})
 
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		list := jobPool.ListWaitingJobs()
+		fmt.Fprintf(w, "%v", list)
+	})
+
 	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) {
 
 		newJob := &MyJob{
-			commandName: "YES",
+			commandName: "echo",
 		}
-		jobPool.QueueJob(newJob.commandName, newJob, 0)
+		_, jobId := jobPool.QueueJob(newJob.commandName, newJob, 0)
+		fmt.Fprintf(w, "%v", jobId)
 	})
 
 	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) {
 		newJob2 := &MyJob{
-			commandName: "THAT",
+			commandName: "wrong",
 		}
-		jobPool.QueueJob(newJob2.commandName, newJob2, 1)
+		_, jobId := jobPool.QueueJob(newJob2.commandName, newJob2, 1)
+		fmt.Fprintf(w, "%v", jobId)
 	})
 
 	http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) {
 
 		newJob3 := &MyJob{
-			commandName: "ROCKS",
+			commandName: "test",
 		}
-		jobPool.QueueJob(newJob3.commandName, newJob3, 1)
+		_, jobId := jobPool.QueueJob(newJob3.commandName, newJob3, 1)
+		fmt.Fprintf(w, "%v", jobId)
 	})
 
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		if q["id"] != nil {
+			searchParam := q["id"][0]
+			j, err := jobPool.GetJobFromJobId(searchParam)
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			} else {
+				jobson := j.GetJobInfos()
+				fmt.Fprintf(w, "%v", string(jobson))
+			}
+		} else {
+			fmt.Fprintf(w, "No search request")
+		}
+	})
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
