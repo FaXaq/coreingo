eb975cbc4d127a3344a782f89cb80ad5fc7ef62c
Marin Procureur <marin.procureur@enioka.com>
2016-06-08 18:58:18 +0200

    Now using iris for ws

---
+++ main2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/config"
+	"github.com/FaXaq/gjp"
+	"log"
+	"fmt"
+)
+
+//global config variables
+var (
+	CallbackStart string
+	CallbackEnd   string
+	WorkPath      string
+	LogPath       string
+)
+
+func main() {
+	err := GetEnvConfig()
+
+	if err != nil {
+		log.Fatal(err.Error()) //if not config then break
+	}
+
+	//create job pool
+	jobPool := gjp.New(3)
+	jobPool.Start()
+
+
+	api := iris.New()
+
+	restConfig := config.Rest {
+		IndentJSON : true,
+	}
+
+	api.Config().Render.Rest = restConfig
+
+	api.Get("/", func(c *iris.Context) {
+		TestPing(c)
+	})
+
+	api.Get("/job/progress", func(c *iris.Context) {
+		GetMyJobProgress(c, jobPool)
+	})
+
+	api.Post("/job", func(c *iris.Context) {
+		CreateJob(c, jobPool)
+	})
+
+	api.Listen(":1337")
+}
