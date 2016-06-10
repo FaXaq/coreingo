3054cd511ddd1cf36592ca3374dcf79ad70dac3b
Marin Procureur <marin.procureur@gmail.com>
2016-06-10 14:21:43 +0200

    Prod run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-export GJP_STARTJOB_CALLBACK=http://localhost:3000/notifications/task
-export GJP_ENDJOB_CALLBACK=http://localhost:3000/notifications/task
-export GJP_LOG_PATH=/var/log/gjp/
-export GJP_WORK_PATH=/home/marin/work/
+export GJP_STARTJOB_CALLBACK=http://54.93.117.237:3000/notifications/task
+export GJP_ENDJOB_CALLBACK=http://54.93.117.237:3000/notifications/task
+export GJP_LOG_PATH=/var/log/core/
+export GJP_WORK_PATH=/workdir/
 export GJP_PORT=1337
 
 go run *.go
